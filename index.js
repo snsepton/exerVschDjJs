93c474d14ba1e6c30dfed53fb2d7918092940b08
snsepton <sonja.septon@utah.edu>
2021-11-16 20:31:46 -0700

    DJJS

---
+++ index.js
@@ -0,0 +1,32 @@
+var box = document.getElementById("box")
+box.style.backgroundColor = "silver"
+
+box.addEventListener('mouseover', function(){
+    box.style.backgroundColor = "blue"
+})
+box.addEventListener('mousedown', function(){
+    box.style.backgroundColor = "red"
+})
+box.addEventListener('mouseup', function(){
+    box.style.backgroundColor = "yellow"
+})
+box.addEventListener('dblclick', function(){
+    box.style.backgroundColor = "green"
+})
+
+box.addEventListener('mouseleave', function(){
+    box.style.backgroundColor = "orange"
+    })
+
+document.addEventListener("keydown", (event) =>{
+    if (event.key == "r")
+        box.style.backgroundColor = "red"
+    else if (event.key == "y")
+        box.style.backgroundColor = "yellow"
+    else if (event.key == "g")
+        box.style.backgroundColor = "green"
+    else if (event.key == "o")
+        box.style.backgroundColor = "orange"
+    else if (event.key == "s")
+        box.style.backgroundColor = "silver"
+    })
